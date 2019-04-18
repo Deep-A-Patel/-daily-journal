28fb80743f2091c55c224a56439f6d2d6c3cff2d
Deep Patel <deep.ag123007@gmail.com>
2019-04-18 10:23:37 -0500

    daily journal part-3 is done

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,23 +8,43 @@ const objectsJournalEnter = {
 const JournalEntry = [
     {
         date: "04/11/2019",
-        Concepts: "css",
+        concepts: "css",
         entry: "I learned css today",
         mood: "normal"  
     },
     {
         date: "04/12/2019",
-        Concepts: "javascripts",
+        concepts: "javascripts",
         entry: "I learned javascripts today",
         mood: "sad" 
     },
     {
         date: "04/13/2019",
-        Concepts: "objects",
+        concepts: "objects",
         entry: "I learned objects today",
         mood: "happy"   
     }
 ]
 
-JournalEntry.push();
-console.log("JournalEntry", JournalEntry);
+const journal = document.querySelector(".journal");
+
+const makeJournalEntryComponent = (JournalEntry) => {
+
+return `
+
+    <h2>${JournalEntry.date}</h2>
+    <h3>${JournalEntry.concepts}</h3>
+    <p>${JournalEntry.entry}</p>
+    <p>${JournalEntry.mood}</p> 
+    
+    `
+}
+
+const renderJournalEntries = (entries) => {
+   entries.forEach(entry => {
+       item = makeJournalEntryComponent(entry)
+       journal.innerHTML += item
+   });
+}
+
+renderJournalEntries(JournalEntry)
