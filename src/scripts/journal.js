a37df3ffe5453d7ac89d8dee94681a41af938ca4
Deep Patel <deep.ag123007@gmail.com>
2019-04-18 12:14:00 -0500

    daily journal part-4 is done

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,50 +1,19 @@
-const objectsJournalEnter = {
-    date: "04/10/2019",
-    Concepts: "html",
-    entry: "I learned html today",
-    mood: "happy"
-};
 
-const JournalEntry = [
-    {
-        date: "04/11/2019",
-        concepts: "css",
-        entry: "I learned css today",
-        mood: "normal"  
-    },
-    {
-        date: "04/12/2019",
-        concepts: "javascripts",
-        entry: "I learned javascripts today",
-        mood: "sad" 
-    },
-    {
-        date: "04/13/2019",
-        concepts: "objects",
-        entry: "I learned objects today",
-        mood: "happy"   
-    }
-]
+const jEntry = document.querySelector(".jEntry");
 
-const journal = document.querySelector(".journal");
-
-const makeJournalEntryComponent = (JournalEntry) => {
-
-return `
-
-    <h2>${JournalEntry.date}</h2>
-    <h3>${JournalEntry.concepts}</h3>
-    <p>${JournalEntry.entry}</p>
-    <p>${JournalEntry.mood}</p> 
-    
-    `
-}
-
-const renderJournalEntries = (entries) => {
-   entries.forEach(entry => {
-       item = makeJournalEntryComponent(entry)
-       journal.innerHTML += item
-   });
-}
-
-renderJournalEntries(JournalEntry)
+fetch("http://localhost:3000/entries")
+    .then(function(response) {
+        
+        return response.json()
+    })
+    .then(function(journalData) {
+        
+        journalData.forEach(journal => {
+            jEntry.innerHTML += `
+            <h2>${journal.date}</h2>
+            <h3>${journal.concepts}</h3>
+            <p>${journal.entry}</p>
+            <p>${journal.mood}</p> 
+            `
+        });
+    })
